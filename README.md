4c54a90a1f1589d2be66fc5f9a9dd01860f08ee6
lisibo <865474355@qq.com>
2019-07-11 11:58:39 +0800

    add hot update

---
+++ README.md
@@ -0,0 +1,7 @@
+#express+mysql
+1.cd express+mysql
+2.cnpm i
+3.npm run dev
+
+nodemon是express热更新文件
+
--- app.js
+++ app.js
@@ -38,4 +38,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// module.exports = app;
+var debug = require('debug')('my-application'); // debug模块
+app.set('port', process.env.PORT || 3000); // 设定监听端口
+ 
+//启动监听
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
