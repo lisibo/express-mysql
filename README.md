0ccb03c54d0183a71163018154c10c88b2fe5c58
lisibo <865474355@qq.com>
2019-07-11 17:15:35 +0800

    update style

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 2.cnpm i
 3.npm run dev
 
+gitignore是git规则配置文件
 nodemon是express热更新文件
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/',(req,res)=>{
+  // if(req.cookies.isLogin){
+  //   req.session.isLogin = req.cookies.isLogin
+  // }
+  // if(req.session.isLogin){
+  //   res.locals.
+  // }
+  res.render('index',{title:"首页",username:req.cookies.username})
+})
+
+router.route('/registe').get((req,res)=>{
+  res.render('registe',{title:"注册"})
+}).post((req,res)=>{
+  res.send('你是谁')
+})
+
+router.route('/login').get((req,res)=>{
+  res.render('login',{title:"登录"})
+}).post((req,res)=>{
+  res.send('你是猪')
+})
+  
 
 module.exports = router;
